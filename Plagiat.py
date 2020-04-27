31e67f753634c990e69220b395e6a62b120d9321
kassacs <48911709+kassacs@users.noreply.github.com>
2020-04-28 00:58:00 +0500

    Update Plagiat.py
    Базовая часть с тестами

--- Plagiat.py
+++ Plagiat.py
@@ -1,9 +1,36 @@
+
 class Plagiat:
-    def __init__(self , p):
-        pass
+    def __init__(self, tekst, tekstcheck, quantity2): 
+        self.quantity2 = quantity2
+        self.tekst = tekst
+        self.tekstcheck = tekstcheck
+    def compare (tekst, tekstcheck, quantity2):
+         return (100 % *q/quantity2)
 class Text:
-    def __init__(self , t):
-    
+    def __init__(self , quantity):
+        self.quantity = quantity
+    def tokenize(self, quantity):
+        return textlist
+        return quantity
 
-
-print ( Plagiat ( Text ())
+ textBase = " В лесу родилась елочка. В лесу она росла.
+                 Зимой и летом стройная, зеленая была!
+                 Метель ей пела песенку - Спи елочка , бай бай!
+                 Мороз снежком укутывал смотри не замерзай!"
+print ( Plagiat.compare (Text (textBase), Text(stih)))
+if __name__  == "__main__":
+    stih = " В лесу родилась елочка. В лесу она росла."
+    stih = Text()
+    print (stih.tokenize())
+    print ( Plagiat.compare (Text (textBase), Text(stih)))
+    stih = " Посадил дед репку. Выросла репка большая."
+    print (stih.tokenize())
+    print ( Plagiat.compare (Text (textBase), Text(stih)))
+    stih = Text()
+    stih = " Смеркалось?"
+    print (stih.tokenize())
+    print ( Plagiat.compare (Text (textBase), Text(stih)))
+    stih = Text()
+    stih = "Метель ей пела песенку - Спи елочка , бай бай!"
+    print (stih.tokenize())
+    print ( Plagiat.compare (Text (textBase), Text(stih)))
