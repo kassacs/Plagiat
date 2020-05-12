a356211b6f25169e8c20a2dc277b6c5112d2cb99
kassacs <48911709+kassacs@users.noreply.github.com>
2020-05-13 02:58:25 +0500

    Update Plagiat.py
    Уточнено понятие плагиата, с использованием функции map и lambda

--- Plagiat.py
+++ Plagiat.py
@@ -1,36 +1,33 @@
-
+from nltk.tokenize import sent_tokenize
 class Plagiat:
-    def __init__(self, tekst, tekstcheck, quantity2): 
-        self.quantity2 = quantity2
+    def __init__(self, tekst, tekstcheck): 
         self.tekst = tekst
         self.tekstcheck = tekstcheck
-    def compare (tekst, tekstcheck, quantity2):
-         return (100 % *q/quantity2)
+    def compare (tekst, tekstcheck):
+         return sum(map(lambda x: tekst.count(x), tekstcheck))/len(tekstcheck)
 class Text:
-    def __init__(self , quantity):
-        self.quantity = quantity
-    def tokenize(self, quantity):
-        return textlist
-        return quantity
-
- textBase = " В лесу родилась елочка. В лесу она росла.
-                 Зимой и летом стройная, зеленая была!
-                 Метель ей пела песенку - Спи елочка , бай бай!
-                 Мороз снежком укутывал смотри не замерзай!"
+    def __init__(self ,text):
+        self.text = text
+    def tokenize(self, text):
+        return sent_tokenize(text, 'russian')
+textBase = "В лесу родилась елочка. В лесу она росла.
+            Зимой и летом стройная, зеленая была!
+            Метель ей пела песенку - Спи елочка , бай бай!
+            Мороз снежком укутывал смотри не замерзай!"
 print ( Plagiat.compare (Text (textBase), Text(stih)))
 if __name__  == "__main__":
-    stih = " В лесу родилась елочка. В лесу она росла."
+    stih = " В лесу родилась елочка. В лесу она росла. Но не выросла"
     stih = Text()
     print (stih.tokenize())
-    print ( Plagiat.compare (Text (textBase), Text(stih)))
+    print ( Plagiat.compare (Text.tokenize(textBase), Text.tokenize(stih)))
     stih = " Посадил дед репку. Выросла репка большая."
     print (stih.tokenize())
-    print ( Plagiat.compare (Text (textBase), Text(stih)))
+    print ( Plagiat.compare (Text.tokenize(textBase), Text.tokenize(stih)))
     stih = Text()
     stih = " Смеркалось?"
     print (stih.tokenize())
-    print ( Plagiat.compare (Text (textBase), Text(stih)))
+    print ( Plagiat.compare (Text.tokenize(textBase), Text.tokenize(stih)))
     stih = Text()
     stih = "Метель ей пела песенку - Спи елочка , бай бай!"
     print (stih.tokenize())
-    print ( Plagiat.compare (Text (textBase), Text(stih)))
+    print ( Plagiat.compare (Text.tokenize(textBase), Text.tokenize(stih)))
