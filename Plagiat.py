68890041a4c06d4ab2eb24d9bc70503add88e167
kassacs <48911709+kassacs@users.noreply.github.com>
2020-05-25 14:43:03 +0500

    Update Plagiat.py
    Добавлен вывод автора и наименования  статьи откуда произошло заимствование

--- Plagiat.py
+++ Plagiat.py
@@ -1,33 +1,42 @@
 from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from pprint import pprint
 class Plagiat:
     def __init__(self, tekst, tekstcheck): 
         self.tekst = tekst
         self.tekstcheck = tekstcheck
     def compare (tekst, tekstcheck):
-         return sum(map(lambda x: tekst.count(x), tekstcheck))/len(tekstcheck)
+        return sum(map(lambda x: tekst.count(x), tekstcheck))/len(tekstcheck)
+    def result (tekst, tekstcheck):
+        if self.compare(tekst, tekstcheck) > 0.5 :
+            pprint ( " Заимствование без корректного оформления ссылки ", Text.author(textBase), Text.title(textBase), self.compare(tekst, tekstcheck))
+        else : print ( "Ваше произведение уникально !" )   
+            
 class Text:
     def __init__(self ,text):
         self.text = text
     def tokenize(self, text):
         return sent_tokenize(text, 'russian')
-textBase = "В лесу родилась елочка. В лесу она росла.
-            Зимой и летом стройная, зеленая была!
-            Метель ей пела песенку - Спи елочка , бай бай!
-            Мороз снежком укутывал смотри не замерзай!"
-print ( Plagiat.compare (Text (textBase), Text(stih)))
+    def author (self, text):
+        if "Автор" in word_tokenize(text, 'russian'): 
+            return word_tokenize(text, 'russian')[word_tokenize(text, 'russian').index ("Автор")+1:word_tokenize(text, 'russian').index ("Автор")+3:]
+    def title (self, text):
+        if "Наименование" in sent_tokenize(text, 'russian'): 
+            return sent_tokenize(text, 'russian')[sent_tokenize(text, 'russian').index ("Наименование")+2]               
+textBase = open('Лень и энтропия.txt', 'r')
 if __name__  == "__main__":
-    stih = " В лесу родилась елочка. В лесу она росла. Но не выросла"
-    stih = Text()
-    print (stih.tokenize())
-    print ( Plagiat.compare (Text.tokenize(textBase), Text.tokenize(stih)))
+    textcheck = open('Лень и энтропия_цитата.txt', 'r')
+    textcheck = Text()
+    print (textcheck.tokenize())
+    print ( Plagiat.result (Text.tokenize(textBase), Text.tokenize(textcheck)))
     stih = " Посадил дед репку. Выросла репка большая."
     print (stih.tokenize())
-    print ( Plagiat.compare (Text.tokenize(textBase), Text.tokenize(stih)))
+    print ( Plagiat.result (Text.tokenize(textBase), Text.tokenize(stih)))
     stih = Text()
     stih = " Смеркалось?"
     print (stih.tokenize())
-    print ( Plagiat.compare (Text.tokenize(textBase), Text.tokenize(stih)))
+    print ( Plagiat.result (Text.tokenize(textBase), Text.tokenize(stih)))
     stih = Text()
     stih = "Метель ей пела песенку - Спи елочка , бай бай!"
     print (stih.tokenize())
-    print ( Plagiat.compare (Text.tokenize(textBase), Text.tokenize(stih)))
+    print ( Plagiat.result (Text.tokenize(textBase), Text.tokenize(stih)))
